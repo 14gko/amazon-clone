966b3ef36402c55c4cb9d6fdf06186c8a4f11694
14gko <your_email@example.com>
2023-10-13 08:28:57 -0400

    add nav

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA45MCbTQbQQtm69r6Pq8WVGVTE5E0CgW0",
+    authDomain: "clone-a8fe3.firebaseapp.com",
+    projectId: "clone-a8fe3",
+    storageBucket: "clone-a8fe3.appspot.com",
+    messagingSenderId: "985347842619",
+    appId: "1:985347842619:web:4b83c6f60c9b7796bdb21e",
+    measurementId: "G-LKRTJZZ7J1"
+  };
